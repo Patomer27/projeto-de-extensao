8d4fda885e5d553eae7cc740a0e0cfb61848d850
Luidi <luidisouza2611@gmail.com>
2026-06-17 20:26:55 -0300

    add java files
    exemplos de variáveis em java adicionados

---
+++ exemplos/java/dados.java
@@ -0,0 +1,26 @@
+public class dados {
+    public static void main(String[] args) {
+
+        // Variável do tipo String (armazena textos)
+        String nome = "Pedro";
+
+        // Variável do tipo int (armazena números inteiros)
+        int idade = 20;
+
+        // Variável do tipo double (armazena números decimais)
+        double altura = 1.78;
+
+        // Variável do tipo char (armazena apenas um caractere)
+        char sexo = 'M';
+
+        // Variável do tipo boolean (armazena verdadeiro ou falso)
+        boolean estudante = true;
+
+        // Exibindo os valores das variáveis
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Estudante: " + estudante);
+    }
+}
---
+++ exemplos/java/jogo.java
@@ -0,0 +1,21 @@
+public class jogo {
+    public static void main(String[] args) {
+
+        // String -> nome do jogador
+        String jogador = "Vinicíus";
+
+        // int -> quantidade de pontos
+        int pontos = 1500;
+
+        // double -> porcentagem de progresso
+        double progresso = 75.5;
+
+        // boolean -> indica se o jogador está online
+        boolean online = true;
+
+        System.out.println(jogador);
+        System.out.println(pontos);
+        System.out.println(progresso);
+        System.out.println(online);
+    }
+}
