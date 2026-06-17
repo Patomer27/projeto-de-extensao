ce389d3953c198970c2a147cb396b39107582653
Luidi <luidisouza2611@gmail.com>
2026-06-17 20:31:45 -0300

    add javascript file
    exemplo de variaveis em javascript

---
+++ exemplos/javascript/variaveis.js
@@ -0,0 +1,11 @@
+// Declarando variáveis com 'let'
+let produto = "Chocolate em Barra";
+let preco_unitario = 7.50;
+let quantidade = 3;
+
+// Processamento
+let valor_total = preco_unitario * quantidade;
+
+// Saída de dados no console
+console.log(`Produto: ${produto}`);
+console.log(`Valor Total: R$ ${valor_total}`);
