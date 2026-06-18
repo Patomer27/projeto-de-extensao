0d0a8f4109923effcfdd3d3befe07a281e74ccfb
Luidi <luidisouza2611@gmail.com>
2026-06-17 21:17:58 -0300

    fix: corrige arquivo PY com código javascript
    corrigida inconsistência entre a extensão do arquivo e a linguagem implementada

--- exercicios/python/Consumo.py
+++ exercicios/python/Consumo.py
@@ -1,31 +1,19 @@
-/*
+"""
 ====================================
 EXERCÍCIO 1 - CONSUMO DE COMBUSTÍVEL
 ====================================
 
 Peça a distância percorrida e a quantidade
 de combustível consumida. Calcule o consumo médio.
-*/
+"""
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+# double (float) -> distância percorrida
+distancia = float(input("Digite a distância percorrida (km): "))
 
-readline.question("Digite a distância percorrida (km): ", (distanciaResp) => {
-    readline.question("Digite os litros consumidos: ", (litrosResp) => {
-        
-        // double -> distância percorrida
-        let distancia = parseFloat(distanciaResp);
+# double (float) -> combustível consumido
+litros = float(input("Digite os litros consumidos: "))
 
-        // double -> combustível consumido
-        let litros = parseFloat(litrosResp);
+# double (float) -> cálculo do consumo médio
+consumo = distancia / litros
 
-        // double -> cálculo do consumo médio
-        let consumo = distancia / litros;
-
-        console.log("Consumo médio: " + consumo + " km/l");
-
-        readline.close();
-    });
-});
+print("Consumo médio: " + str(consumo) + " km/l")
